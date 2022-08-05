cc5a303f4fd8c3d65a6ad8c37ed2d611508a89b6
Mabel Lee <leemabel.711@gmail.com>
2022-08-05 17:28:56 +0200

    serve a HTML file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
 
-app.get("/", function (req, res) {
-    res.send("Hello Express")
-})
+// console.log("Hello World");
+
+// app.get("/", function (req, res) {
+//     res.send("Hello Express")
+// })
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+} )
 
 
 
