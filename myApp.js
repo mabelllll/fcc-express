8328cfd31a64c0ebb5eac59b24c233ced4b7900e
Mabel Lee <leemabel.711@gmail.com>
2022-08-09 16:27:04 +0200

    chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -5,12 +5,18 @@ let absolutePath = '/views/index.html'
 let absolutePath1 = '/public'
 var message = "Hello json"
 
-
 // console.log("Hello World");
 
 // app.get("/", function (req, res) {
 //     res.send("Hello Express")
 // })
+
+app.use((req, res, next) => {
+    let logger = req.method + " " + req.path + " - " + req.ip
+    console.log(logger)
+    console.log(req.ip)
+    next()
+})
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + absolutePath)
@@ -18,7 +24,7 @@ app.get("/", (req, res) => {
 
 app.use('/public', express.static(__dirname + absolutePath1))
 
-app.get("/json", (req,res) => {
+app.get("/json", (req, res) => {
     if(process.env.MESSAGE_STYLE === "uppercase") {
         res.json({
             "message": message.toUpperCase()
@@ -28,11 +34,14 @@ app.get("/json", (req,res) => {
             "message": message
         })
     }
-    
 })
 
-console.log(process.env['MESSAGE_STYLE']);
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+}, (req, res) => {
+    res.json({time : req.time})
+})
 
 
 
