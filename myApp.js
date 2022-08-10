36c02356606c3ef6044261720dcedc2f7d2414fa
Mabel Lee <leemabel.711@gmail.com>
2022-08-10 11:50:01 +0200

    use body-parser to parse POST request & get data from POST requests

--- myApp.js
+++ myApp.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
-let express = require('express');
+let express = require('express')
 let app = express();
+let bodyParser = require('body-parser')
 let absolutePath = '/views/index.html'
 let absolutePath1 = '/public'
-var message = "Hello json"
+var messageString = "Hello json"
 
 // console.log("Hello World");
 
 // app.get("/", function (req, res) {
 //     res.send("Hello Express")
 // })
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use((req, res, next) => {
     let logger = req.method + " " + req.path + " - " + req.ip
@@ -27,11 +30,11 @@ app.use('/public', express.static(__dirname + absolutePath1))
 app.get("/json", (req, res) => {
     if(process.env.MESSAGE_STYLE === "uppercase") {
         res.json({
-            "message": message.toUpperCase()
+            messageString: message.toUpperCase()
         })
     } else {
         res.json({
-            "message": message
+            messageString: message
         })
     }
 })
@@ -47,6 +50,35 @@ app.get('/:word/echo', (req, res) => {
     res.json({"echo": req.params.word})
 })
 
+/*
+
+ALTERNATIVE OPTION BELOW
+
+app.get('/name', (req, res) => {
+    const firstname = req.query.first
+    const lastname = req.query.last
+    res.json({
+        name: firstname + " " + lastname
+    })
+})
+
+*/
+
+app.route('/name').get((req,res) => {
+    const firstname = req.query.first
+    const lastname = req.query.last
+    res.json({
+        name: firstname + " " + lastname
+    })
+})
+
+app.post('/name', (req, res) => {
+    const firstname = req.body.first
+    const lastname = req.body.last
+    res.json({
+        name: firstname + " " + lastname
+    })
+})
 
 
 
